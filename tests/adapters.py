ddd96f7012fd527a3d1399cebb8202febf7286e1
liyuheng <liyuheng55555@126.com>
2026-04-26 23:03:43 +0800

    softmax

--- tests/adapters.py
+++ tests/adapters.py
@@ -20,6 +20,7 @@ from ch2.bpe_train import bpe_train
 from ch3.Embedding import Embedding
 
 from ch3.Linear import Linear
+from ch3.Softmax import softmax
 
 
 def run_linear(
@@ -451,7 +452,7 @@ def run_softmax(in_features: Float[Tensor, " ..."], dim: int) -> Float[Tensor, "
         Float[Tensor, "..."]: Tensor of with the same shape as `in_features` with the output of
         softmax normalizing the specified `dim`.
     """
-    raise NotImplementedError
+    return softmax(in_features, dim)
 
 
 def run_cross_entropy(
