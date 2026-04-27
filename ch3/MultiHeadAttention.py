326b1bc313e4bae366810a8451869c9f1995b5a2
liyuheng <liyuheng55555@126.com>
2026-04-28 01:56:25 +0800

    rope upgrade, implement MultiHeadAttentionWithRope

--- ch3/MultiHeadAttention.py
+++ ch3/MultiHeadAttention.py
@@ -1,8 +1,9 @@
 import einops
 import torch
-from jaxtyping import Float
+from jaxtyping import Float, Int
 from torch import Tensor, nn
 
+from ch3.RotaryPositionalEmbedding import RotaryPositionalEmbedding
 from ch3.ScaledDotProductAttention import scaled_dot_product_attention
 
 
@@ -48,7 +49,57 @@ class MultiHeadAttention(nn.Module):
         return result
 
 
+class MultiHeadAttentionWithRope(nn.Module):
+    def __init__(
+            self,
+            d_model: int,
+            num_heads: int,
+            max_seq_len: int,
+            theta: float,
+            q_proj_weight: Float[Tensor, " d_model d_model"],
+            k_proj_weight: Float[Tensor, " d_model d_model"],
+            v_proj_weight: Float[Tensor, " d_model d_model"],
+            o_proj_weight: Float[Tensor, " d_model d_model"],
+    ):
+        super().__init__()
+        self.d_model = d_model
+        self.num_heads = num_heads
+        assert q_proj_weight.shape[0] % num_heads == 0
+        self.d_k = q_proj_weight.shape[0] // num_heads
+        self.W_q : nn.Parameter = nn.Parameter(q_proj_weight)
+        self.W_k : nn.Parameter = nn.Parameter(k_proj_weight)
+        self.W_v : nn.Parameter = nn.Parameter(v_proj_weight)
+        self.W_o : nn.Parameter = nn.Parameter(o_proj_weight)
+        self.rope = RotaryPositionalEmbedding(self.d_k, max_seq_len, theta)
 
+
+    def forward(
+            self,
+            in_features: Float[Tensor, " ... sequence_length d_model"],
+            token_positions: Int[Tensor, " ... sequence_length"] | None = None,
+    ) -> Float[Tensor, " ... sequence_length d_model"]:
+        multi_q = einops.einsum(in_features, self.W_q, "... seq_len d_model, d_multi_q d_model -> ... seq_len d_multi_q")
+        multi_k = einops.einsum(in_features, self.W_k, "... seq_len d_model, d_multi_k d_model -> ... seq_len d_multi_k")
+        multi_v = einops.einsum(in_features, self.W_v, "... seq_len d_model, d_multi_v d_model -> ... seq_len d_multi_v")
+
+        multi_q = einops.rearrange(multi_q, "... seq_len (heads d_q) -> ... heads seq_len d_q", heads=self.num_heads)
+        multi_k = einops.rearrange(multi_k, "... seq_len (heads d_k) -> ... heads seq_len d_k", heads=self.num_heads)
+        multi_v = einops.rearrange(multi_v, "... seq_len (heads d_v) -> ... heads seq_len d_v", heads=self.num_heads)
+
+        rope_q = self.rope.forward(multi_q, token_positions)
+        rope_k = self.rope.forward(multi_k, token_positions)
+
+        seq_len : int = in_features.shape[-2]
+
+        mask = torch.tril(torch.ones(seq_len, seq_len, dtype=torch.bool))
+
+        attention = scaled_dot_product_attention(rope_q, rope_k, multi_v, mask)
+
+        concat = einops.rearrange(attention, "... heads seq_len d_v -> ... seq_len (heads d_v)") # heads * d_v == d_model
+
+        result = einops.einsum(concat, self.W_o, "... seq_len d_model, d_out d_model-> ... seq_len d_out")
+
+        return result
 
 
 
--- ch3/RotaryPositionalEmbedding.py
+++ ch3/RotaryPositionalEmbedding.py
@@ -1,49 +1,75 @@
 import math
+
+import einops
 import torch
 from typing import Optional
 
+from jaxtyping import Float, Int
+from torch import Tensor
 
 
 class RotaryPositionalEmbedding(torch.nn.Module):
 
 
-    def __init__(self, theta: float, d_k: int, max_seq_len: int, device=None):
+    def __init__(self, d_k: int, max_seq_len: int, theta: float = 10000.0, device=None):
         super().__init__()
         self.theta = theta
         self.d = d_k
         self.max_seq_len = max_seq_len
 
-        self.cos = torch.zeros(max_seq_len, self.d)
-        self.sin = torch.zeros(max_seq_len, self.d)
+        # [max_seq_len, self.d]
+        self.cos = torch.zeros(max_seq_len, self.d // 2)
+        self.sin = torch.zeros(max_seq_len, self.d // 2)
         self.cal_cos_sin()
-
 
 
     def cal_cos_sin(self):
         for i in range(self.max_seq_len):
-            for k in range(1, int(self.d / 2) + 2):
-                theta_i_k = i / (self.theta ** ((2*(k - 1)) / self.d))
+            for k in range(0, self.d // 2):
+                theta_i_k = i / (self.theta ** ((2*k) / self.d))
                 self.cos[i][k] = math.cos(theta_i_k)
                 self.sin[i][k] = math.sin(theta_i_k)
 
 
-    def forward(self, x: torch.Tensor, token_positions: torch.Tensor):
-        x_flat = x.flatten()
-        seq_len = x.shape[-2]
-        result = torch.zeros(x.numel())
-        base_index = 0
-        while base_index < x.numel() / x.shape[-1]:
-            position = token_positions.flatten()[base_index % token_positions.numel()]
-            global_index = base_index * self.d
-            for g in range(0, self.d, 2):
-                k = int(g/2) + 1
-                a = x_flat[global_index + g]
-                b = x_flat[global_index + g + 1]
-                result[global_index + g] = a * self.cos[position][k] - b * self.sin[position][k]
-                result[global_index + g + 1] = a * self.sin[position][k] + b * self.cos[position][k]
-            base_index += 1
+    def forward(
+            self,
+            x: Float[Tensor, " ... sequence_length d_k"],
+            token_positions: Int[Tensor, " ... sequence_length"],
+    ) -> Float[Tensor, " ... sequence_length d_k"]:
+        # 需要想想怎么优雅化
+        COS = self.cos[token_positions] # [... sequence_length self.d // 2]
+        SIN = self.sin[token_positions]
 
-        result_reshape = result.reshape(x.shape)
-        return result_reshape
+        a = x[..., ::2]
+        b = x[..., 1::2]
+
+        a_cos = a * COS
+        a_sin = a * SIN
+        b_cos = b * COS
+        b_sin = b * SIN
+
+        g = a_cos - b_sin
+        g_1 = a_sin + b_cos
+
+        G = torch.stack([g, g_1], dim=-1) # ... sequence_length, self.d // 2, 2
+        result = einops.rearrange(G, "... half_d two -> ... (half_d two)")
+        return result
+
+        # x_flat = x.flatten()
+        # result = torch.zeros(x.numel())
+        # base_index = 0
+        # while base_index < x.numel() / x.shape[-1]:
+        #     position = token_positions.flatten()[base_index % token_positions.numel()]
+        #     global_index = base_index * self.d
+        #     for g in range(0, self.d, 2):
+        #         k = int(g/2) + 1
+        #         a = x_flat[global_index + g]
+        #         b = x_flat[global_index + g + 1]
+        #         result[global_index + g] = a * self.cos[position][k] - b * self.sin[position][k]
+        #         result[global_index + g + 1] = a * self.sin[position][k] + b * self.cos[position][k]
+        #     base_index += 1
+        #
+        # result_reshape = result.reshape(x.shape)
+        # return result_reshape
 
 
